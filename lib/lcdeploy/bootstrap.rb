b21721aa513bfef09030261640bb6d073fbe9a7c
axocomm <axocomm@gmail.com>
2017-08-21 19:04:40 -0400

    Add LCDFile.preview method for displaying commands to run

--- lib/lcdeploy/bootstrap.rb
+++ lib/lcdeploy/bootstrap.rb
@@ -1,17 +1,29 @@
 require 'lcdeploy/resources'
 
 def create_directory(name, params = {})
-  puts LCD::Resources.create_directory(name, params)
+  cmd = LCD::Resources.create_directory(name, params)
+  if @dry_run
+    puts cmd
+  end
 end
 
 def clone_repository(source, params = {})
-  puts LCD::Resources.clone_repository(source, params)
+  cmd = LCD::Resources.clone_repository(source, params)
+  if @dry_run
+    puts cmd
+  end
 end
 
 def build_docker_image(name, params = {})
-  puts LCD::Resources.build_docker_image(name, params)
+  cmd = LCD::Resources.build_docker_image(name, params)
+  if @dry_run
+    puts cmd
+  end
 end
 
 def run_docker_container(name, params = {})
-  puts LCD::Resources.run_docker_container(name, params)
+  cmd = LCD::Resources.run_docker_container(name, params)
+  if @dry_run
+    puts cmd
+  end
 end
